44c030e8d1acb474a9d88ce15cc0afcbb7de031d
DavMort <davidmortensen90@gmail.com>
2023-04-28 09:32:39 +0200

    laptop

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,12 +1,40 @@
 package org.example;
 import java.util.*;
+import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
 
     }
 
+    public static int findShort(String s) {
+        List<String> sList = new ArrayList<>(Arrays.stream(s.split(" ")).toList());
+        int small = sList.get(0).length();
+        for (String str : sList) {
+            small = Math.min(small, str.length());
+        }
+        return small;
+    }
 
+    public static String rps(String p1, String p2) {
+        // your code
+        if (p1.equals(p2)) {
+            return "Draw!";
+        }
+        if ((p1.equals("rock") && p2.equals("scissors")) || (p1.equals("scissors") && p2.equals("paper")) || (p1.equals("paper") && p2.equals("rock"))) {
+            return "Player 1 won!";
+        } else {
+            return "Player 2 won!";
+        }
+    }
+
+    public static String highAndLow(String numbers) {
+        // Code here or
+        String[] sArr = numbers.split(" ");
+        List<Integer> sList = new ArrayList<>(Arrays.stream(numbers.split(" ")).toList().stream().map(Integer::parseInt).toList());
+        Collections.sort(sList);
+        return String.format("%d %d", sList.get(sList.size() - 1), sList.get(0));
+    }
 
     public static int countDuplicate(List<Integer> numbers) {
         List<Integer> temp = new ArrayList<>();
@@ -109,5 +137,68 @@ public class Main {
                 System.out.println(i);
             }
         }
+    }
+
+    public static List<String> number(List<String> lines) {
+        List<String> result = new ArrayList<>(lines.size());
+        for (int i = 0; i < result.size(); i++) {
+            result.add((i + 1) + ": " + lines.get(i));
+        }
+        return result;
+    }
+    public static int howOld(final String herOld) {
+
+        //your code here, return correct age as int ; )
+        return Integer.parseInt(herOld.substring(0, 1));
+    }
+
+    public static Integer basicMath(String op, int v1, int v2) {
+        switch (op) {
+            case "+" -> {
+                return v1 + v2;
+            }
+            case "-" -> {
+                return v1 - v2;
+            }
+            case "*" -> {
+                return v1 * v2;
+            }
+            case "/" -> {
+                return v1 / v2;
+            }
+            default -> {
+                return null;
+            }
+        }
+    }
+    public static int areaOrPerimeter (int l, int w) {
+        // code away...
+        if (l == w) {
+            return l * w;
+        }
+        return l * 2 + w * 2;
+    }
+
+    public static boolean betterThanAverage(int[] classPoints, int yourPoints) {
+        // Your code here
+            return (double)yourPoints > Arrays.stream(classPoints).average().getAsDouble();
+    }
+    public static String findNeedle(Object[] haystack) {
+        // Your code here
+        int index = 0;
+        for (Object o : haystack) {
+            if (o.equals("needle")) {
+                return "found the needle at position " + index;
+            }
+            index++;
+        }
+        return null;
+    }
+
+    public static String oddOrEven (int[] array) {
+        // your code
+        // if empty consider array as 0 "even"
+        return array.length == 0 || Arrays.stream(array).sum() % 2 == 0 ? "even" : "odd";
+
     }
 }
